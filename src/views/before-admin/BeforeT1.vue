1f2f69ef0c5fcc993032ecec5c2f65751bdf7291
Huxuan.Gong@fintern.ai <741247407@qq.com>
2020-11-11 20:27:33 +0800

    frist commit

---
+++ src/views/before-admin/BeforeT1.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="before-t1">
+    new
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BeforeT1',
+  components: {},
+  props: {},
+  data() {
+    return {
+    }
+  },
+  created() {},
+  methods: {}
+}
+</script>
+<style lang="less" scoped>
+</style>
